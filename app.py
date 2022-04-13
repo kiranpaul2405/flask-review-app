73b8b3f7cabfe848b48bb5c947d9f2b430a2eae1
kiranpaul2405 <101860377+kiranpaul2405@users.noreply.github.com>
2022-04-14 01:33:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask,request,jsonify
 import joblib
 import pandas as pd
-
+from flask_cors import CORS
 
 
 app=Flask(__name__)
-
+CORS(app)
 @app.route('/predict',methods=['POST'])
 
 def predict():
